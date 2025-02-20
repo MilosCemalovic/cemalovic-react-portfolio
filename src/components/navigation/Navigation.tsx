38c6b55979a3df4f0652a7a7fc5df0aaaa755f0e
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-02-20 12:48:58 +0100

    Moving initial images

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,7 +1,28 @@
-const Navigation = () => {
-  return (
-    <div>Navigation</div>
-  )
+import styles from "./Navigation.module.scss";
+
+interface NavigationItem {
+  id: string;
+  label: string;
 }
 
-export default Navigation
+const Navigation = () => {
+  // const NAV_ITEMS: NavigationItem[] = [
+  //   { id: 'experience', label: 'Experience' },
+  //   { id: 'skills', label: 'Skills' },
+  //   { id: 'projects', label: 'Projects' },
+  //   { id: 'about', label: 'About' },
+  //   { id: 'contact', label: 'Contact' }
+  // ]
+
+  return (
+    <nav>
+      <img
+        src="cemalovic-milos-portfolio-img-1.jpg"
+        alt="Milos Cemalovic"
+        className={styles.profileImage}
+      />
+    </nav>
+  );
+};
+
+export default Navigation;
