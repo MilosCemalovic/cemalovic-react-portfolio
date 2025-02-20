5bdf348ab001d3bea7d9a92c6ff4e855407aa96e
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-02-20 13:09:50 +0100

    Added react-scroll and inital sidebar image with links

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,28 +1,47 @@
-import styles from "./Navigation.module.scss";
+import { Link } from "react-scroll"
+import profileImage from "../../assets/img/cemalovic-milos-portfolio-img-1.jpg"
+import styles from "./Navigation.module.scss"
 
 interface NavigationItem {
-  id: string;
-  label: string;
+  id: string
+  label: string
 }
 
 const Navigation = () => {
-  // const NAV_ITEMS: NavigationItem[] = [
-  //   { id: 'experience', label: 'Experience' },
-  //   { id: 'skills', label: 'Skills' },
-  //   { id: 'projects', label: 'Projects' },
-  //   { id: 'about', label: 'About' },
-  //   { id: 'contact', label: 'Contact' }
-  // ]
+  const NAV_ITEMS: NavigationItem[] = [
+    { id: "experience", label: "Experience" },
+    { id: "skills", label: "Skills" },
+    { id: "projects", label: "Projects" },
+    { id: "about", label: "About" },
+    { id: "contact", label: "Contact" },
+  ]
 
   return (
     <nav>
       <img
-        src="cemalovic-milos-portfolio-img-1.jpg"
+        src={profileImage}
         alt="Milos Cemalovic"
         className={styles.profileImage}
       />
-    </nav>
-  );
-};
 
-export default Navigation;
+      <ul className="list-unstyled">
+        {NAV_ITEMS.map((item) => (
+          <li key={item.id}>
+            <Link
+              className={styles.navLink}
+              to={item.id}
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
