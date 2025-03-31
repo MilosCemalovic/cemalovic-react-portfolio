cc7aed12ffa165b6fea60709a885251e6950cf57
Milos Cemalovic <miloscemalovicnew@gmail.com>
2025-03-31 20:26:34 +0200

    Hobbies removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import About from "./components/About/About"
 import Experience from "./components/Experience/Experience"
 import Hero from "./components/Hero/Hero"
-import Hobbies from "./components/Hobbies/Hobbies"
 import Navigation from "./components/Navigation/Navigation"
 import Projects from "./components/Projects/Projects"
 import Skills from "./components/Skills/Skills"
@@ -18,7 +17,6 @@ const App = () => {
         <Skills />
         <Projects />
         <About />
-        <Hobbies />
       </main>
 
       <Contact />
